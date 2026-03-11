0d029e4f2d4dd647964906359c6df7d943318efd
Asheesh Rawat <asheeshrawat48@gmail.com>
2026-03-11 06:07:37 +0530

    added cors package ans its fucn

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -3,10 +3,10 @@ import { Redis } from "@upstash/redis";
 import dotenv from "dotenv";
 dotenv.config();
 //create a ratelimiter that allows 100 request per 60 sec.
-
+// https://console.upstash.com/redis
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(100, "60 s"),
+  limiter: Ratelimit.slidingWindow(10, "20 s"),
 });
 
 export default ratelimit;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,24 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 
 import { connectDB } from "./config/db.js";
 import notesRoutes from "./routes/notesRoutes.js";
-import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
+
+const app = express();
 dotenv.config();
 // console.log(process.env.MONGO_URI);
 const port = process.env.PORT || 3000;
-const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 app.use(express.json());
 app.use(rateLimiter);
+
 //custom middleware
 // app.use((req, res, next) => {
 //   console.log("req method is :", req.method , "req url is :" ,  req.url);
