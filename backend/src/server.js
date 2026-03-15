a83c171d0730108014c1c988d539a240825c449b
Asheesh Rawat <asheeshrawat48@gmail.com>
2026-03-15 11:11:35 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import cors from "cors";
+import path from "path";
 
 import { connectDB } from "./config/db.js";
 import notesRoutes from "./routes/notesRoutes.js";
@@ -10,12 +11,17 @@ import rateLimiter from "./middleware/rateLimiter.js";
 const app = express();
 // console.log(process.env.MONGO_URI);
 const port = process.env.PORT || 3000;
+const __dirname = path.resolve();
+// console.log(__dirname); D:\notes\100Xdev\mern-notes-app\backend
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -28,9 +34,20 @@ app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
 // An endpoint is a specific URL + HTTP method combination that your server listens on and responds to.
 
-connectDB().then(() => {
-  // Start server
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+//serving optimised react app(dist) as static asset.
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
-});
+}
+
+connectDB()
+  .then(() =>
+    app.listen(port, () => console.log(`Server running on port ${port}`)),
+  )
+  .catch((err) => {
+    console.error("DB connection failed:", err);
+    process.exit(1);
+  });
